27666a2ca9cd05f729dd3f96470cda679b8843ec
liuwei9 <liuwei199805@yeah.net>
2021-08-16 21:26:39 +0800

    update leaky_relu

--- src/main/scala/image_leaky_relu.scala
+++ src/main/scala/image_leaky_relu.scala
@@ -1,5 +1,6 @@
 import spinal.core._
 
+//下一步让进来的数据不经过zero模块
 class image_leaky_relu(
                           S_DATA_WIDTH: Int,
                           ZERO_POINT_WIDTH: Int,
@@ -12,7 +13,17 @@ class image_leaky_relu(
         val data_out = out Bits (M_DATA_WIDTH bits)
     }
     noIoPrefix()
+    val LEAKY_S_DATA_WIDTH = S_DATA_WIDTH/COMPUTE_CHANNEL_OUT_NUM
+    val LEAKY_M_DATA_WIDTH = M_DATA_WIDTH/COMPUTE_CHANNEL_OUT_NUM
+    val leaky_clk = ClockDomain(this.clockDomain.clock)
+    var leaky_list:List[leaky_relu]=Nil
+    for (_ <- 0 until COMPUTE_CHANNEL_OUT_NUM) {
+        leaky_list = leaky_clk(new leaky_relu(LEAKY_S_DATA_WIDTH,ZERO_POINT_WIDTH,LEAKY_M_DATA_WIDTH))::leaky_list
+    }
+    leaky_list = leaky_list.reverse
     for (i <- 0 until COMPUTE_CHANNEL_OUT_NUM) {
-
+        leaky_list(i).io.data_in <> io.data_in((i+1)*LEAKY_S_DATA_WIDTH -1 downto i*LEAKY_S_DATA_WIDTH)
+        leaky_list(i).io.zero_data_in <> io.zero_data_in
+        leaky_list(i).io.data_out <> io.data_out((i+1)*LEAKY_S_DATA_WIDTH -1 downto i*LEAKY_S_DATA_WIDTH)
     }
 }

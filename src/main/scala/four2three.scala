c6e75bc0cb7d808a493c8a5843e10b8dec273b56
liuwei9 <liuwei199805@yeah.net>
2021-08-31 17:33:42 +0800

    update 43

--- src/main/scala/four2three.scala
+++ src/main/scala/four2three.scala
@@ -1,6 +1,7 @@
 import spinal.core._
 import spinal.lib._
 import spinal.lib.fsm._
+import xmemory.sdpram
 
 class four2three(
                     S_DATA_WIDTH: Int,
@@ -23,18 +24,26 @@ class four2three(
     }
     noIoPrefix()
     val Channel_Times = io.Channel_In_Num_REG.asUInt >> 3
-    val count_mult = new mul(ROW_COL_DATA_COUNT_WIDTH, Channel_Times.getWidth, ROW_COL_DATA_COUNT_WIDTH,false)
+    val count_mult = new mul(ROW_COL_DATA_COUNT_WIDTH, Channel_Times.getWidth, ROW_COL_DATA_COUNT_WIDTH, false)
     count_mult.io.A := io.Row_Num_After_Padding
     count_mult.io.B := Channel_Times.asBits
 
 
-    val fifo = new general_fifo_sync(S_DATA_WIDTH,S_DATA_WIDTH,MEMORY_DEPTH,ROW_COL_DATA_COUNT_WIDTH)
+    val fifo = new general_fifo_sync(S_DATA_WIDTH, S_DATA_WIDTH, MEMORY_DEPTH, ROW_COL_DATA_COUNT_WIDTH)
     fifo.io.data_in <> io.S_DATA.payload
     fifo.io.wr_en <> io.S_DATA.valid
     fifo.io.data_in_ready <> io.S_DATA.ready
     fifo.io.m_data_count <> count_mult.io.P.asUInt
     fifo.io.s_data_count <> count_mult.io.P.asUInt
 
+    val ram1 = new sdpram(S_DATA_WIDTH, MEMORY_DEPTH, S_DATA_WIDTH, MEMORY_DEPTH, "distributed", 0, clka = this.clockDomain, clkb = this.clockDomain)
+    val ram2 = new sdpram(S_DATA_WIDTH, MEMORY_DEPTH, S_DATA_WIDTH, MEMORY_DEPTH, "distributed", 0, clka = this.clockDomain, clkb = this.clockDomain)
+    val ram3 = new sdpram(S_DATA_WIDTH, MEMORY_DEPTH, S_DATA_WIDTH, MEMORY_DEPTH, "distributed", 0, clka = this.clockDomain, clkb = this.clockDomain)
+    val ram4 = new sdpram(S_DATA_WIDTH, MEMORY_DEPTH, S_DATA_WIDTH, MEMORY_DEPTH, "distributed", 0, clka = this.clockDomain, clkb = this.clockDomain)
+    ram1.io.addrb <> io.M_Addr.asBits.resized
+    ram2.io.addrb <> io.M_Addr.asBits.resized
+    ram3.io.addrb <> io.M_Addr.asBits.resized
+    ram4.io.addrb <> io.M_Addr.asBits.resized
 
     val f2t_fsm = new StateMachine {
         val IDLE = new State() with EntryPoint
@@ -59,6 +68,47 @@ class four2three(
             init_en := False
         }
 
+        val Cnt_Cin = UInt(CHANNEL_NUM_WIDTH bits) setAsReg()
+        val EN_Last_Cin = Bool()
+        when(Cnt_Cin === Channel_Times - 1) {
+            EN_Last_Cin := True
+        } otherwise {
+            EN_Last_Cin := False
+        }
+        when(isActive(Read)) {
+            when(EN_Last_Cin) {
+                Cnt_Cin := 0
+            } otherwise {
+                Cnt_Cin := Cnt_Cin + 1
+            }
+        } otherwise {
+            Cnt_Cin := 0
+        }
+
+        val Cnt_Column = UInt(ROW_COL_DATA_COUNT_WIDTH bits) setAsReg()
+        val EN_Read_State = Bool()
+        when(Cnt_Column === io.Row_Num_After_Padding - 1) {
+            EN_Read_State := True
+        } otherwise {
+            EN_Read_State := False
+        }
+        when(isActive(Read)) {
+            when(EN_Last_Cin) {
+                Cnt_Column := Cnt_Column + 1
+            } otherwise {
+                Cnt_Column := Cnt_Column
+            }
+        } otherwise {
+            Cnt_Column := 0
+        }
+
+
+        val Cnt_Ram = UInt(2 bits) setAsReg() init 0
+        when(isEntering(Judge_Compute)) {
+            Cnt_Ram := Cnt_Ram + 1
+        } elsewhen isEntering(Start_Compute) {
+            Cnt_Ram := Cnt_Ram - 1
+        } otherwise (Cnt_Ram := Cnt_Ram)
 
 
         IDLE
@@ -68,16 +118,36 @@ class four2three(
                 } otherwise goto(IDLE)
             }
         INIT
-            .whenIsActive{
-                when(init_en){
+            .whenIsActive {
+                when(init_en) {
                     goto(Judge_Fifo)
                 } otherwise goto(INIT)
             }
         Judge_Fifo
-            .whenIsActive{
-                when(fifo.io.data_out_valid){
+            .whenIsActive {
+                when(fifo.io.data_out_valid) {
                     goto(Read)
                 } otherwise goto(Judge_Fifo)
             }
+        Read
+            .whenIsActive {
+                when(EN_Read_State && EN_Last_Cin) {
+                    goto(Judge_Compute)
+                } otherwise goto(Read)
+            }
+        Judge_Compute
+            .whenIsActive {
+                when(Cnt_Ram === 3) {
+                    when(io.M_Ready) {
+                        goto(Start_Compute)
+                    } otherwise goto(Judge_Compute)
+
+                } otherwise goto(Judge_Fifo)
+            }
+        Start_Compute
+            .whenIsActive {
+
+            }
+
     }
 }

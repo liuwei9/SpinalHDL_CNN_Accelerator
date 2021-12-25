42b88127a291ec11d8d68ed010e4e618aec2930b
liuwei9 <liuwei199805@yeah.net>
2021-12-25 15:53:06 +0800

    update conv state

--- src/main/scala/conv/Conv.scala
+++ src/main/scala/conv/Conv.scala
@@ -1,6 +1,6 @@
 package conv
 
-import conv.ConvCtrlEnum.newElement
+import conv.ConvCtrlEnum.{IDLE, newElement}
 import spinal.core._
 import spinal.lib._
 import spinal.lib.fsm._
@@ -124,6 +124,19 @@ case class ConvCtrl(convConfig: ConvConfig) extends Component {
 }
 
 class Conv(convConfig: ConvConfig) extends Component {
+    val io = new Bundle {
+        val sData = Vec(slave Stream UInt(convConfig.FEATURE_S_DATA_WIDTH bits), convConfig.KERNEL_NUM)
+        val mData = master Stream UInt(convConfig.FEATURE_M_DATA_WIDTH bits)
+        val startCu = in Bool()
+        val startPa = in Bool()
+
+
+    }
+    noIoPrefix()
+
+    val convState = ConvState(convConfig)
+    val loadWeight = LoadWeight(convConfig)
+
 
 }
 
@@ -169,6 +182,13 @@ case class LoadWeightFsm(start: Bool) extends Area {
                 nextState := LoadWeightEnum.COPY_BIAS
             }
         }
+        is(LoadWeightEnum.COPY_SCALE) {
+            when(copyScaleEnd) {
+                nextState := LoadWeightEnum.COPY_SHIFT
+            } otherwise {
+                nextState := LoadWeightEnum.COPY_SCALE
+            }
+        }
         is(LoadWeightEnum.COPY_SHIFT) {
             when(copyShiftEnd) {
                 nextState := LoadWeightEnum.IDLE
@@ -185,7 +205,7 @@ case class LoadWeight(convConfig: ConvConfig) extends Component {
         val sData = slave Stream UInt(convConfig.WEIGHT_S_DATA_WIDTH bits)
         val weightNum = in UInt (log2Up(convConfig.WEIGHT_S_DATA_DEPTH) bits)
         val quanNum = in UInt (log2Up(convConfig.QUAN_S_DATA_DEPTH) bits)
-        val weightRead = WeightReadPort(UInt(convConfig.WEIGHT_M_DATA_WIDTH bits), convConfig.WEIGHT_M_DATA_DEPTH)
+        val weightRead = Vec(WeightReadPort(UInt(convConfig.WEIGHT_M_DATA_WIDTH bits), convConfig.WEIGHT_M_DATA_DEPTH), convConfig.KERNEL_NUM)
         val biasRead = WeightReadPort(UInt(convConfig.QUAN_M_DATA_WIDTH bits), convConfig.QUAN_M_DATA_DEPTH)
         val scaleRead = WeightReadPort(UInt(convConfig.QUAN_M_DATA_WIDTH bits), convConfig.QUAN_M_DATA_DEPTH)
         val shiftRead = WeightReadPort(UInt(convConfig.QUAN_M_DATA_WIDTH bits), convConfig.QUAN_M_DATA_DEPTH)
@@ -198,51 +218,198 @@ case class LoadWeight(convConfig: ConvConfig) extends Component {
     val copyWeightTimes = WaCounter(copyWeightCnt.valid, convConfig.KERNEL_NUM.toBinaryString.length, convConfig.KERNEL_NUM)
     fsm.copyWeightEnd := copyWeightCnt.valid && copyWeightTimes.valid
 
+    when(fsm.currentState === LoadWeightEnum.COPY_WEIGHT || fsm.currentState === LoadWeightEnum.COPY_SHIFT || fsm.currentState === LoadWeightEnum.COPY_BIAS || fsm.currentState === LoadWeightEnum.COPY_SCALE) {
+        io.sData.ready := True
+    } otherwise {
+        io.sData.ready := False
+    }
+
+
+    val weav = Vec(Bool(), convConfig.KERNEL_NUM)
+    when(io.sData.fire) {
+        switch(copyWeightTimes.count) {
+            (0 until convConfig.KERNEL_NUM).foreach(i => {
+                is(i) {
+                    (0 until convConfig.KERNEL_NUM).foreach(j => {
+                        if (i == j) {
+                            weav(j) := True
+                        } else {
+                            weav(j) := False
+                        }
+                    }
+                    )
+                }
+            })
+            default {
+                weav.map(_ := False)
+            }
+
+        }
+
+    } otherwise {
+        weav.map(_ := False)
+    }
     val weightRam = Array.tabulate(convConfig.KERNEL_NUM) { i =>
         def gen = {
             val temp = new sdpram(convConfig.WEIGHT_S_DATA_WIDTH, convConfig.WEIGHT_S_DATA_DEPTH, convConfig.WEIGHT_M_DATA_WIDTH, convConfig.WEIGHT_M_DATA_DEPTH, MEM_TYPE.block, 1, CLOCK_MODE.common_clock, this.clockDomain, this.clockDomain)
-            temp.io.wea <> io.sData.fire.asBits
-            when(copyWeightTimes.count === i) {
-                temp.io.ena := True
-            } otherwise {
-                temp.io.ena := False
-            }
+            temp.io.wea <> weav(i).asBits
+            temp.io.ena := True
             temp.io.enb := True
             temp.io.addra := copyWeightCnt.count.asBits
-            temp.io.addrb := io.weightRead.addr.asBits
+            temp.io.addrb := io.weightRead(i).addr.asBits
             temp.io.dina <> io.sData.payload.asBits
-            temp.io.doutb <> io.weightRead.data.asBits
+            temp.io.doutb.asUInt <> io.weightRead(i).data
             temp
         }
 
         gen
     }
 
-    case class copyQuan(enCnt: Bool,wea:Bool,dina:UInt,addrb:UInt,doutb:UInt,clk:ClockDomain) extends Area {
+    case class copyQuan(enCnt: Bool, wea: Bool, dina: UInt, addrb: UInt, doutb: UInt, clk: ClockDomain) extends Area {
         val copyCnt = WaCounter(enCnt, log2Up(convConfig.QUAN_S_DATA_DEPTH), io.quanNum - 1)
-        val ram = new sdpram(convConfig.QUAN_S_DATA_WIDTH,convConfig.QUAN_S_DATA_DEPTH,convConfig.QUAN_M_DATA_WIDTH,convConfig.QUAN_M_DATA_DEPTH,MEM_TYPE.distributed,0,CLOCK_MODE.common_clock,clk,clk)
+        val ram = new sdpram(convConfig.QUAN_S_DATA_WIDTH, convConfig.QUAN_S_DATA_DEPTH, convConfig.QUAN_M_DATA_WIDTH, convConfig.QUAN_M_DATA_DEPTH, MEM_TYPE.distributed, 0, CLOCK_MODE.common_clock, clk, clk)
         ram.io.ena <> True
         ram.io.wea <> wea.asBits
         ram.io.dina <> dina.asBits
         ram.io.addra <> copyCnt.count.asBits
         ram.io.addrb <> addrb.asBits
-        ram.io.doutb <> doutb.asBits
+        ram.io.doutb.asUInt <> doutb
         ram.io.enb <> True
     }
 
-    val copyBias = copyQuan(fsm.currentState===LoadWeightEnum.COPY_BIAS&&io.sData.fire,fsm.currentState===LoadWeightEnum.COPY_BIAS&&io.sData.fire,io.sData.payload,io.biasRead.addr,io.biasRead.data,this.clockDomain)
+    val copyBias = copyQuan(fsm.currentState === LoadWeightEnum.COPY_BIAS && io.sData.fire, fsm.currentState === LoadWeightEnum.COPY_BIAS && io.sData.fire, io.sData.payload, io.biasRead.addr, io.biasRead.data, this.clockDomain)
     fsm.copyBiasEnd := copyBias.copyCnt.valid
-    val copyScale = copyQuan(fsm.currentState===LoadWeightEnum.COPY_SCALE&&io.sData.fire,fsm.currentState===LoadWeightEnum.COPY_SCALE&&io.sData.fire,io.sData.payload,io.scaleRead.addr,io.scaleRead.data,this.clockDomain)
+    val copyScale = copyQuan(fsm.currentState === LoadWeightEnum.COPY_SCALE && io.sData.fire, fsm.currentState === LoadWeightEnum.COPY_SCALE && io.sData.fire, io.sData.payload, io.scaleRead.addr, io.scaleRead.data, this.clockDomain)
     fsm.copyScaleEnd := copyScale.copyCnt.valid
-    val copyShift = copyQuan(fsm.currentState===LoadWeightEnum.COPY_SHIFT&&io.sData.fire,fsm.currentState===LoadWeightEnum.COPY_SHIFT&&io.sData.fire,io.sData.payload,io.shiftRead.addr,io.shiftRead.data,this.clockDomain)
+    val copyShift = copyQuan(fsm.currentState === LoadWeightEnum.COPY_SHIFT && io.sData.fire, fsm.currentState === LoadWeightEnum.COPY_SHIFT && io.sData.fire, io.sData.payload, io.shiftRead.addr, io.shiftRead.data, this.clockDomain)
     fsm.copyShiftEnd := copyShift.copyCnt.valid
 
 
 }
 
+object ConvStateEnum extends SpinalEnum(defaultEncoding = binaryOneHot) {
+    val IDLE, PARA, PARA_IRQ, COMPUTE, COMPUTE_IRQ = newElement
+}
+
+object CONV_STATE extends Area {
+    val START_PA = 1
+    val START_CU = 2
+    val END_IRQ = 15
+
+    val END_PA = 1
+    val END_CU = 2
+
+    val IDLE_STATE = 0
+    val PARA_STATE = 1
+    val COMPUTE_STATE = 2
+    val IRQ_STATE = 15
+
+    val PARA_SIGN = 1
+    val COMPUTE_SIGN = 2
+    val IDLE_SIGN = 0
+}
+
+case class ConvStateFsm(control: Bits, complete: Bits) extends Area {
+
+    //    val control = Bits(4 bits)
+    //    val complete = Bits(4 bits)
+
+    val currentState = Reg(ConvStateEnum()) init ConvStateEnum.IDLE
+    val nextState = ConvStateEnum()
+    currentState := nextState
+    switch(currentState) {
+        is(ConvStateEnum.IDLE) {
+            when(control === CONV_STATE.START_PA) {
+                nextState := ConvStateEnum.PARA
+            } elsewhen (control === CONV_STATE.START_CU) {
+                nextState := ConvStateEnum.COMPUTE
+            } otherwise {
+                nextState := ConvStateEnum.IDLE
+            }
+        }
+        is(ConvStateEnum.PARA) {
+            when(complete === CONV_STATE.END_PA) {
+                nextState := ConvStateEnum.PARA_IRQ
+            } otherwise {
+                nextState := ConvStateEnum.PARA
+            }
+        }
+        is(ConvStateEnum.PARA_IRQ) {
+            when(control === CONV_STATE.END_IRQ) {
+                nextState := ConvStateEnum.IDLE
+            } otherwise {
+                nextState := ConvStateEnum.PARA_IRQ
+            }
+        }
+        is(ConvStateEnum.COMPUTE) {
+            when(complete === CONV_STATE.END_CU) {
+                nextState := ConvStateEnum.COMPUTE_IRQ
+            } otherwise {
+                nextState := ConvStateEnum.COMPUTE
+            }
+        }
+        is(ConvStateEnum.COMPUTE_IRQ) {
+            when(control === CONV_STATE.END_IRQ) {
+                nextState := ConvStateEnum.IDLE
+            } otherwise {
+                nextState := ConvStateEnum.COMPUTE_IRQ
+            }
+        }
+
+    }
+}
+
+case class ConvState(convConfig: ConvConfig) extends Component {
+    val io = new Bundle {
+        val control = in Bits (4 bits)
+        val complete = in Bits (4 bits)
+        val state = out(Reg(Bits(4 bits)))
+        val sign = out(Reg(Bits(4 bits)))
+        val dmaReadValid = out Bool()
+        val dmaWriteValid = out Bool()
+    }
+    noIoPrefix()
+    val fsm = ConvStateFsm(io.control, io.complete)
+    switch(fsm.currentState) {
+        is(ConvStateEnum.IDLE) {
+            io.state := CONV_STATE.IDLE_STATE
+        }
+        is(ConvStateEnum.PARA) {
+            io.state := CONV_STATE.PARA_STATE
+        }
+        is(ConvStateEnum.COMPUTE) {
+            io.state := CONV_STATE.COMPUTE_STATE
+        }
+        is(ConvStateEnum.COMPUTE_IRQ) {
+            io.state := CONV_STATE.IRQ_STATE
+        }
+        is(ConvStateEnum.PARA_IRQ) {
+            io.state := CONV_STATE.IRQ_STATE
+        }
+//        default {
+//            io.state := CONV_STATE.IDLE_STATE
+//        }
+    }
+
+    when(fsm.currentState === ConvStateEnum.IDLE && fsm.nextState === ConvStateEnum.PARA) {
+        io.sign := CONV_STATE.PARA_SIGN
+        io.dmaReadValid.set()
+        io.dmaWriteValid.clear()
+    } elsewhen (fsm.currentState === ConvStateEnum.IDLE && fsm.nextState === ConvStateEnum.COMPUTE) {
+        io.sign := CONV_STATE.COMPUTE_SIGN
+        io.dmaWriteValid.set()
+        io.dmaReadValid.set()
+    } otherwise {
+        io.sign := CONV_STATE.IDLE_SIGN
+        io.dmaReadValid.clear()
+        io.dmaWriteValid.clear()
+    }
+
+
+}
 
 object Conv {
     def main(args: Array[String]): Unit = {
-        SpinalVerilog(new LoadWeight(ConvConfig(8,16,8,12,2048,512,640,2048,ConvType.conv33)))
+        SpinalVerilog(ConvState(ConvConfig(8, 16, 8, 12, 2048, 512, 640, 2048, ConvType.conv33)))
     }
 }
